4ddf019502c70e21ca650d6f8e5bf1b87b74a0c3
Estiven Moreno <estiven@gmail.com>
2026-04-17 02:32:47 -0500

    mensaje de cambios

--- mini_siga/app.py
+++ mini_siga/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -9,16 +10,17 @@ def crear_bd():
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS aulas (
-                   id INTEGER PRIMARY KEY AUTOINCREMENT,
-                   nombre TEXT,
-                   capacidad INTERGER)
-    """ )
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,
+            capacidad INTEGER
+        )
+    """)
     
     conexion.commit()
     conexion.close()
 
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def inicio():
     conexion = sqlite3.connect("siga.db")
     cursor = conexion.cursor()
@@ -26,7 +28,6 @@ def inicio():
     if request.method == "POST":
         nombre = request.form["nombre"]
         capacidad = request.form["capacidad"]
-
 
         cursor.execute("INSERT INTO aulas (nombre, capacidad) VALUES (?, ?)", (nombre, capacidad))
         conexion.commit()
@@ -36,8 +37,9 @@ def inicio():
 
     conexion.close()
 
-    return render_template("index.html", aulas=aulas)  
-crear_bd()
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html", aulas=aulas)
 
+crear_bd()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
